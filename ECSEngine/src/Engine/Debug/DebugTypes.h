cad9d672c7a906c6dad6a1c75fdc06a56395aae6
むにまる <munimaru62ooo@gmail.com>
2026-06-14 16:50:22 +0900

    ci: fix .gitignore to include source debug folder and add missing files

---
+++ ECSEngine/src/Engine/Debug/DebugTypes.h
@@ -0,0 +1,51 @@
+﻿// Copyright (c) 2026 munimaru62o. All rights reserved.
+
+#pragma once
+
+#include "Engine/Math/Color.h"
+#include "Engine/Math/Vector3.h"
+
+
+namespace Debug
+{
+
+struct Line
+{
+    Vector3 start = {};
+    Vector3 end = {};
+    Color color = Color::White();
+};
+
+namespace Config
+{
+constexpr bool IsEnabled = false;
+constexpr int EntitySamplingInterval = 1000;
+}
+namespace Overlay
+{
+inline bool IsVisible = true;
+}
+namespace Draw3D
+{
+inline bool IsVisible = true;
+}
+
+namespace Scale
+{
+constexpr float Force = 0.1f;
+}
+
+namespace DrawColor
+{
+namespace Force
+{
+constexpr Color Attraction  = Color::Red();
+constexpr Color Swirl       = Color::Cyan();
+constexpr Color Lift        = Color::Green();
+constexpr Color Boids       = Color::Blue();
+constexpr Color Wander      = Color::Yellow();
+constexpr Color Dart        = Color::Magenta();
+constexpr Color Boundary    = Color::White();
+}
+}
+}
